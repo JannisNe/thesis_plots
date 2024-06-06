7f81e61abfbaff9e33a0b3822c5f2c4c65ee375e
JannisNe <necker@physik.hu-berlin.de>
2024-06-06 17:25:10 +0200

    add z axis

--- thesis_plots/flaires/kcorrection.py
+++ thesis_plots/flaires/kcorrection.py
@@ -14,7 +14,7 @@ from thesis_plots.plotter import Plotter
 logger = logging.getLogger(__name__)
 
 
-@Plotter.register(["side", "hist"])
+@Plotter.register("side")
 def kcorrection():
     logger.info("plotting k-correction")
 
@@ -32,6 +32,16 @@ def kcorrection():
     fits = [np.polyfit(fit_wavelengths, np.log10(f[fit_mask]), 1) for f in fluxes]
     logger.info(f"fitted spectral indices: {[fi[0] for fi in fits]}")
 
+    def zfromwavelength(w):
+        z = (WISEDataBase.band_wavelengths["W1"].to("AA")).value / w - 1
+        logger.debug(f"z from wavelength {w} is {z}")
+        return z
+
+    def wavelengthfromz(z):
+        w = WISEDataBase.band_wavelengths["W1"].to("AA").value / (1 + z)
+        logger.debug(f"wavelength from z {z} is {w}")
+        return w
+
     fig, ax = plt.subplots()
     ax.plot(wavelengths, bb_flux, label=f"Blackbody, {T} K")
     for i, (f, fit) in enumerate(zip(fluxes, fits)):
@@ -42,9 +52,18 @@ def kcorrection():
     ax.axvline(WISEDataBase.band_wavelengths["W2"].to("AA").value, color="red", ls=":")
     ax.set_xscale("log")
     ax.set_yscale("log")
-    ax.set_xlim((1e3, 1e5))
+    ax.set_xlim((5e3, 1e5))
     ax.set_ylim((1e-6, 1e1))
     ax.set_xlabel("Wavelength [AA]")
     ax.set_ylabel("Flux [a.u.]")
-    ax.legend()
+    zax = ax.twiny()
+    zax.set_xscale("log")
+    zax.set_xlim(ax.get_xlim())
+    zs = np.array([0, 1, 2, 3, 4, 5])
+    zax.set_xticks([wavelengthfromz(z) for z in zs])
+    zax.set_xticks([wavelengthfromz(z) for z in zs + 0.5], minor=True)
+    zax.set_xticklabels([f"{z:.0f}" for z in zs])
+    zax.set_xticklabels([], minor=True)
+    zax.set_xlabel("Redshift")
+    ax.legend(ncols=2, loc="lower center", bbox_to_anchor=(0.5, 1.2), frameon=False)
     return fig
