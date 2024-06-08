57612098b374b94cad4df202004629573b6abad4
JannisNe <necker@physik.hu-berlin.de>
2024-06-09 00:28:36 +0200

    using cache

--- thesis_plots/flaires/kcorrection.py
+++ thesis_plots/flaires/kcorrection.py
@@ -9,33 +9,35 @@ import numpy as np
 import pandas as pd
 
 from thesis_plots.plotter import Plotter
+from thesis_plots.cache import DiskCache
 
 
 logger = logging.getLogger(__name__)
 
 
-@Plotter.register("side")
-def kcorrection():
-    logger.info("plotting k-correction")
-
+@DiskCache.cache
+def kcorrection_data(temp: float, fit_wave=(1e4, 1e5)):
     k = Kcorrect(responses=["sdss_g0", "twomass_Ks", "wise_w1", "wise_w2"])
     wavelengths = k.templates.restframe_wave
     fluxes = [pd.Series(f / max(f)).rolling(100).median() for f in k.templates.restframe_flux]
-    T = 2000
-    bb = BlackBody(temperature=T * u.K)
+    bb = BlackBody(temperature=temp * u.K)
     bb_flux = (bb(wavelengths * u.AA) * const.c / (wavelengths * u.AA)**2).to("erg s-1 cm-2 AA-1 sr-1")
     bb_flux = bb_flux / max(bb_flux)
 
-    fit_wave = (1e4, 1e5)
+    # fit_wave = (1e4, 1e5)
     fit_mask = (wavelengths > fit_wave[0]) & (wavelengths < fit_wave[1])
     fit_wavelengths = np.log10(wavelengths[fit_mask])
     fits = [np.polyfit(fit_wavelengths, np.log10(f[fit_mask]), 1) for f in fluxes]
-    logger.info(f"fitted spectral indices: {[fi[0] for fi in fits]}")
+    return wavelengths, fluxes, bb_flux, fits, fit_wave
 
-    def zfromwavelength(w):
-        z = (WISEDataBase.band_wavelengths["W1"].to("AA")).value / w - 1
-        logger.debug(f"z from wavelength {w} is {z}")
-        return z
+
+@Plotter.register("margin")
+def kcorrection():
+    logger.info("plotting k-correction")
+
+    temp = 2000
+    wavelengths, fluxes, bb_flux, fits, fit_wave = kcorrection_data(temp=temp)
+    logger.info(f"fitted spectral indices: {[fi[0] for fi in fits]}")
 
     def wavelengthfromz(z):
         w = WISEDataBase.band_wavelengths["W1"].to("AA").value / (1 + z)
@@ -43,16 +45,16 @@ def kcorrection():
         return w
 
     fig, ax = plt.subplots()
-    ax.plot(wavelengths, bb_flux, label=f"Blackbody, {T} K")
+    ax.plot(wavelengths, bb_flux, label=f"Blackbody, {temp} K", zorder=10)
     for i, (f, fit) in enumerate(zip(fluxes, fits)):
-        ax.plot(wavelengths, f, label=f"templates" if i == 0 else None, color="black")
+        ax.plot(wavelengths, f, label=f"templates" if i == 0 else None, color="dimgrey", zorder=2)
         ax.plot(fit_wave, 10**np.polyval(fit, np.log10(fit_wave)), label=f"PL fits" if i == 0 else None, ls="--",
-                color="black", alpha=0.7)
+                color="dimgrey", alpha=0.7, zorder=5)
     ax.axvline(WISEDataBase.band_wavelengths["W1"].to("AA").value, color="red", label="WISE bands", ls=":")
     ax.axvline(WISEDataBase.band_wavelengths["W2"].to("AA").value, color="red", ls=":")
     ax.set_xscale("log")
     ax.set_yscale("log")
-    ax.set_xlim((5e3, 1e5))
+    ax.set_xlim((3e3, 1e5))
     ax.set_ylim((1e-6, 1e1))
     ax.set_xlabel("Wavelength [AA]")
     ax.set_ylabel("Flux [a.u.]")
