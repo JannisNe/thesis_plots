c7d4ff26cd6e5149c44ca50156263f7aec2ec3f3
JannisNe <necker@physik.hu-berlin.de>
2024-06-12 15:16:38 +0200

    add system to download resim files

---
+++ thesis_plots/resimulations/data/resim/create_checksum.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import logging
+from thesis_plots.resimulations.results import SCPDownloader
+
+logger = logging.getLogger("thesis_plots.resimulations.data.create_checksum")
+
+checksum_file = Path(__file__).parent / "checksum.json"
+
+
+def create_checksum():
+    SCPDownloader().create_checksum()
+
+
+if __name__ == "__main__":
+    logging.getLogger("thesis_plots").setLevel(logging.DEBUG)
+    create_checksum()
