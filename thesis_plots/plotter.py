d2cdc839a9c93f5ad286575cd47d183911cd76b1
JannisNe <necker@physik.hu-berlin.de>
2024-06-13 09:42:18 +0200

    add arg loop, ability to only give sub string

--- thesis_plots/plotter.py
+++ thesis_plots/plotter.py
@@ -1,6 +1,9 @@
 import logging
 import os
+from typing import Any
 from pathlib import Path
+
+import numpy as np
 from matplotlib import style
 import matplotlib.pyplot as plt
 from difflib import get_close_matches
@@ -23,19 +26,34 @@ class Plotter:
         self.dir.mkdir(exist_ok=True)
 
     @classmethod
-    def register(cls, style_name: str | list[str] = None):
+    def register(cls, style_name: str | list[str] = None, arg_loop: Any | list[Any] | None = None):
         if isinstance(style_name, str):
             style_name = [style_name]
         _add_styles = [f"thesis_plots.styles.{w}" for w in style_name] if style_name else []
         _styles = ["thesis_plots.styles.base"] + _add_styles
 
         def plot_function_with_style(f):
-            def wrapper(*args, **kwargs):
-                logger.debug(f"using styles {_styles}")
-                style.use(_styles)
-                return f(*args, **kwargs)
 
-            cls.registry[f.__module__.strip("thesis_plots.") + ":" + f.__name__] = wrapper
+            fname = f.__module__.replace("thesis_plots.", "") + ":" + f.__name__
+
+            if not arg_loop:
+                def wrapper(*args, **kwargs):
+                    logger.debug(f"using styles {_styles}")
+                    style.use(_styles)
+                    return f(*args, **kwargs)
+
+                cls.registry[fname] = wrapper
+
+            else:
+                wrappers = []
+                for a in np.atleast_1d(arg_loop):
+                    def wrapper(*args, **kwargs):
+                        logger.debug(f"using styles {_styles}")
+                        style.use(_styles)
+                        return f(a, *args, **kwargs)
+
+                    cls.registry[f"{fname}_{a}"] = wrapper
+                    wrappers.append(wrapper)
 
             return f
 
@@ -45,12 +63,15 @@ class Plotter:
         return self.dir / (name.replace(":", "_").replace(".", "_") + ".pdf")
 
     def plot(self, name: str | list[str] | None = None, save: bool = False, show: bool = False):
+        all_names = self.registry.keys()
         if isinstance(name, str):
             name = [name]
         if name is None:
             name = self.registry.keys()
-        logger.info(f"making {len(name)} plots")
-        for n in name:
+        _name = [n2 for n2 in all_names if any([n2.startswith(n1) for n1 in name])]
+        logger.info(f"making {len(_name)} plots")
+        logger.debug(", ".join(_name))
+        for n in _name:
             if n not in self.registry:
                 raise KeyError(f"plot {n} not found in registry! {_did_you_mean(n, list(self.registry.keys()))}")
             logger.info(f"plotting {n}")
