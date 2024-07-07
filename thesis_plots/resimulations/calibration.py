0b57a8d1a3ca785d3cbeb6b2dac3b68f9c9c35be
JannisNe <necker@physik.hu-berlin.de>
2024-07-07 14:54:43 +0200

    fixx purity and efficiency

--- thesis_plots/resimulations/calibration.py
+++ thesis_plots/resimulations/calibration.py
@@ -42,8 +42,8 @@ def metric_calibration():
     tywin_m = get_max_e_ratio(get_data("tywin"))
     txs_m = get_max_e_ratio(get_data("txs"))
     m_thresh = np.linspace(0, 2, 100)
-    p_tywin = np.sum(tywin_m[:, np.newaxis] < m_thresh, axis=0) / len(tywin_m)
-    p_txs = np.sum(txs_m[:, np.newaxis] > m_thresh, axis=0) / len(txs_m)
+    p_tywin = np.sum(tywin_m[:, np.newaxis] > m_thresh, axis=0) / len(tywin_m)
+    p_txs = np.sum(txs_m[:, np.newaxis] < m_thresh, axis=0) / len(txs_m)
     logger.info("1-CDF(0.8) for IC170922A: {:.2f}".format(sum(txs_m > 0.8) / len(txs_m)))
 
     fig, ax = plt.subplots()
