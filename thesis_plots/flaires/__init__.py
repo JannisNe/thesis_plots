3d15c5e024dac1c257684e0605ec43ab0ab548bd
JannisNe <necker@physik.hu-berlin.de>
2024-07-25 12:21:52 +0200

    add missing modules

--- thesis_plots/flaires/__init__.py
+++ thesis_plots/flaires/__init__.py
@@ -1,1 +1,1 @@
-from thesis_plots.flaires import kcorrection, ngc7392, radius, distributions, colors
+from thesis_plots.flaires import kcorrection, ngc7392, radius, distributions, colors, rate, diagnostics, parent_sample
