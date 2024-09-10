4e11c55076055b4dbcc456a5ea9dbd8a88f5dd7c
JannisNe <necker@physik.hu-berlin.de>
2024-09-10 22:19:24 +0200

    nicer lbel nicer fig

--- thesis_plots/accretion_flare_stacking/diagnostics.py
+++ thesis_plots/accretion_flare_stacking/diagnostics.py
@@ -4,6 +4,8 @@ import json
 import pickle
 from pathlib import Path
 
+from IPython.core.pylabtools import figsize
+
 from thesis_plots.plotter import Plotter
 
 
@@ -21,11 +23,13 @@ def energy_range():
         energy_range_final = pickle.load(f)
 
     ylim = {
-        "1": (8e-16, 1e-13),
-        "2": (4e-9, 2e-8)
+        "1": (8e-16, 1e-14),
+        "2": (4e-9, 1e-8)
     }
 
-    fig, axs = plt.subplots(ncols=2, sharex=True, gridspec_kw={"wspace": 0.3})
+    height = plt.rcParams["figure.figsize"][1] * 2 / 3
+    width = plt.rcParams["figure.figsize"][0]
+    fig, axs = plt.subplots(ncols=2, sharex=True, gridspec_kw={"wspace": 0.3}, figsize=(width, height))
     for ax, (gamma, res) in zip(axs, energy_range_results.items()):
         e_min_gev = res["e_min_gev"]
         e_max_gev = res["e_max_gev"]
@@ -37,7 +41,7 @@ def energy_range():
         low_handle = ax.plot(e_min_gev, sens_min, ls="--", marker="o", ms=2, c="C0")
         hi_handle = ax.plot(e_max_gev, sens_max, ls="-", marker="o", ms=2, c="C0")
         range_handle = ax.fill_betweenx(
-           ylim[gamma], [erange[0]], [erange[1]] , color="black", alpha=0.2, zorder=0, ec="none",
+           ylim[gamma], [erange[0]], [erange[1]], color="black", alpha=0.2, zorder=0, ec="none",
         )
         ax.axhline(nominal_sens, label="nominal sensitivity", color="black")
         ax.set_xscale("log")
@@ -57,7 +61,7 @@ def energy_range():
         "Energy range",
         "Nominal sensitivity",
     ]
-    fig.legend(legend_handles, legend_labels, loc="upper center", ncol=2, bbox_to_anchor=(0.5, 1.05))
+    fig.legend(legend_handles, legend_labels, loc="upper center", ncol=2, bbox_to_anchor=(0.5, 1.1))
 
     fig.supxlabel(r"$E_\mathrm{cut}$ [GeV]", va="top", ha="center")
     axs[0].set_ylabel("Sensitivity flux [GeV$^{-1}$ cm$^{-2}$ s$^{-1}$]")
