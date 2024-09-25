5818a7f90729060afce488c793be02b9f6aeae4a
JannisNe <necker@physik.hu-berlin.de>
2024-09-25 19:42:38 +0200

    add lightcurve

--- thesis_plots/ztf/ZTF19adgzidh.py
+++ thesis_plots/ztf/ZTF19adgzidh.py
@@ -1,17 +1,21 @@
 import logging
 from astropy.io import fits
 import matplotlib.pyplot as plt
+import pandas as pd
 from thesis_plots.plotter import Plotter
-from thesis_plots.ztf.data import data_dir, load_lines
+from thesis_plots.ztf.data import data_dir, load_lines, band_colors
 
 
 logger = logging.getLogger(__name__)
 
 
 z = .4760
+neutrino_time_str = "2022-06-24 16:13:16.41"
+neutrino_time = pd.to_datetime(neutrino_time_str)
+neutrino_name = "IC220624A"
 
 
-@Plotter.register()
+@Plotter.register("half")
 def spectrum():
     data_file = data_dir / "ZTF19adgzidh_coadded1d_spec.fits"
     logger.debug(f"loading data from {data_file}")
@@ -49,4 +53,31 @@ def spectrum():
 
     ax.set_xlim(3500, 9800)
     ax.set_ylim(0, 40)
+    return fig
+
+
+@Plotter.register("half")
+def lightcurve():
+    file = data_dir / "ZTF19adgzidh_photometry.csv"
+    logger.debug(f"loading data from {file}")
+    data = pd.read_csv(file, parse_dates=["created_at", "UTC"])
+    ul_mask = data.mag.isna()
+    band_masks = {b: data["filter"] == b for b in ["ztfg", "ztfr"]}
+
+    fig, ax = plt.subplots()
+    for band, mask in band_masks.items():
+        color = band_colors[band[-1]]
+        band_data = data[mask & ~ul_mask]
+        if len(band_data) == 0:
+            continue
+        ax.errorbar(band_data.UTC, band_data.mag, yerr=band_data.magerr, fmt="o", label=band, color=color, ms=2, elinewidth=0.5)
+    ax.axvline(neutrino_time, color="C0", ls="--", label=neutrino_name)
+    ax.set_xlabel("UTC")
+    ax.set_ylabel("magnitude")
+    for label in ax.get_xticklabels():
+        label.set_rotation(45)
+        label.set_ha("right")
+    ax.invert_yaxis()
+    ax.set_xlim(pd.Timestamp("2022-03"), pd.Timestamp("2022-11"))
+    ax.legend(bbox_to_anchor=(0.5, 1), loc='lower center', ncol=3)
     return fig
