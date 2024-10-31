761fc3af5f46323e59762d76dd36d550fab10bbb
JannisNe <necker@physik.hu-berlin.de>
2024-10-31 15:25:31 +0100

    fix spl

--- thesis_plots/icecube_diffuse_flux/spectrum.py
+++ thesis_plots/icecube_diffuse_flux/spectrum.py
@@ -1,7 +1,6 @@
 import logging
 import abc
 import numpy as np
-from jedi.inference.value.iterable import Sequence
 from mypy.server.objgraph import Iterable
 from numpy import typing as npt
 import pandas as pd
@@ -18,10 +17,13 @@ class Spectrum(abc.ABC):
     def __init__(self,
                  best_fit_parameters: Iterable,
                  energy_range: tuple[float, float],
+                 reference_energy_gev: float,
                  contour_file68: str | Path | None = None,
-                 contour_file95: str | Path | None = None
+                 contour_file95: str | Path | None = None,
+                 csv_kwargs: dict | None = None
                  ):
-        self._best_fit_paramaters = best_fit_parameters
+        self._best_fit_parameters = best_fit_parameters
+        self.reference_energy_gev = reference_energy_gev
         self.energy_range = energy_range
         self.contour_files = {}
         for cl, fn in zip([68, 95], [contour_file68, contour_file95]):
@@ -31,9 +33,10 @@ class Spectrum(abc.ABC):
                 self.contour_files[cl] = p
             else:
                 logger.debug(f"no contour file for {cl} percent")
+        self.csv_kwargs = csv_kwargs if csv_kwargs else {}
 
     @abc.abstractmethod
-    def flux(self, e: npt.NDArray[float], *parameters) -> npt.NDArray[float]:
+    def flux(self, e_gev: npt.NDArray[float], *parameters) -> npt.NDArray[float]:
         pass
 
     @property
@@ -43,20 +46,21 @@ class Spectrum(abc.ABC):
 
     @property
     def best_fit(self):
-        return {k: v for k, v in zip(self.paramater_names, self._best_fit_paramaters)}
+        return {k: v for k, v in zip(self.paramater_names, self._best_fit_parameters)}
 
     def contour(self, cl: float) -> pd.DataFrame:
         assert cl in self.contour_files, f"no contour file for {cl} percent"
-        return pd.read_csv(self.contour_files[cl], delimiter=";", names=self.paramater_names)
+        df = pd.read_csv(self.contour_files[cl], **self.csv_kwargs)
+        return df[self.paramater_names]
 
-    def upper(self, cl: float, e: npt.NDArray[float]) -> npt.NDArray[float]:
-        return np.max(self.flux(e, *self.contour(cl).values.T), axis=0)
+    def upper(self, cl: float, e_gev: npt.NDArray[float]) -> npt.NDArray[float]:
+        return np.max(self.flux(e_gev, *self.contour(cl).values.T), axis=0)
 
-    def lower(self, cl: float, e: npt.NDArray[float]) -> npt.NDArray[float]:
-        return np.min(self.flux(e, *self.contour(cl).values.T), axis=0)
+    def lower(self, cl: float, e_gev: npt.NDArray[float]) -> npt.NDArray[float]:
+        return np.min(self.flux(e_gev, *self.contour(cl).values.T), axis=0)
 
-    def best(self, e: npt.NDArray[float]) -> npt.NDArray[float]:
-        return self.flux(e, *self._best_fit_paramaters)
+    def best(self, e_gev: npt.NDArray[float]) -> npt.NDArray[float]:
+        return self.flux(e_gev, *self.best_fit.values())
 
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
@@ -83,8 +87,8 @@ class SinglePowerLaw(Spectrum):
 
     @property
     def paramater_names(self):
-        return ["norm", "gamma"]
+        return ["gamma", "norm"]
 
-    def flux(self, e: npt.NDArray[float], *parameters) -> npt.NDArray[float]:
-        norm, gamma = parameters
-        return norm * (e ** -gamma)
+    def flux(self, e_gev: npt.NDArray[float], *parameters) -> npt.NDArray[float]:
+        gamma, norm = parameters
+        return norm * (e_gev / self.reference_energy_gev) ** -gamma
