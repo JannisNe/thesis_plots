f8795e1e487a91d088a295840bffbdd127c33423
JannisNe <necker@physik.hu-berlin.de>
2024-10-31 18:05:19 +0100

    fix cascades, add hese

--- thesis_plots/icecube/diffuse.py
+++ thesis_plots/icecube/diffuse.py
@@ -10,22 +10,22 @@ logger = logging.getLogger(__name__)
 
 @Plotter.register()
 def all_measurements():
-    ls = ["-", ":", "--", "-"]
-    fc = ["k", "none", "none", "none"]
-    ec = ["none", "C0", "C1", "C2"]
-    alpha = [0.5, 1, 1, 1]
+    ls = ["-", ":", "--", "-", "-."]
+    fc = ["k", "none", "none", "none", "none"]
+    ec = ["none", "C0", "C1", "C2", "C3"]
+    alpha = [0.5, 1, 1, 1, 1]
 
     fig, ax = plt.subplots()
-    for i, name in enumerate(["joint15", "joint23_spl", "nt22", "hese20"]):
+    for i, name in enumerate(["joint15", "nt22", "joint23_spl"]):
         s = load_spectrum(name)
         srange = s.get_energy_range()
         supper = s.upper(68, srange) * srange ** 2
         slower = s.lower(68, srange) * srange ** 2
-        label = f"{s.journal} ({s.year})"
+        label = f"{s.journal} ({s.year})" if name != "joint23_spl" else s.journal
         ax.fill_between(srange, slower, supper, label=label, fc=fc[i], ec=ec[i], zorder=1, lw=2, alpha=alpha[i], ls=ls[i])
     ax.set_xscale("log")
     ax.set_yscale("log")
     ax.set_xlabel("Energy [GeV]")
     ax.set_ylabel(r"$E^2 \Phi^{\nu + \bar{\nu}}_\mathrm{per \; flavor}$ [GeV cm$^{-2}$ s$^{-1}$ sr$^{-1}$]")
-    ax.legend(ncols=2, bbox_to_anchor=(0.5, 1.01), loc="lower center")
+    ax.legend()
     return fig
