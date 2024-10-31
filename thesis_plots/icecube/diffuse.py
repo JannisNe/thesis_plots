114e0bed82dfb76576e8de95a1b595f4e1e6734d
JannisNe <necker@physik.hu-berlin.de>
2024-10-31 17:07:24 +0100

    nicer plot

--- thesis_plots/icecube/diffuse.py
+++ thesis_plots/icecube/diffuse.py
@@ -10,17 +10,22 @@ logger = logging.getLogger(__name__)
 
 @Plotter.register("wide")
 def all_measurements():
+    ls = ["-", ":", "-.", "-"]
+    fc = ["k", "none", "none", "none"]
+    ec = ["none", "C0", "C1", "C2"]
+    alpha = [0.5, 1, 1, 1]
+
     fig, ax = plt.subplots()
-    for name in ["joint15", "joint23_spl", "nt22", "hese20"]:
+    for i, name in enumerate(["joint15", "joint23_spl", "nt22", "hese20"]):
         s = load_spectrum(name)
         srange = s.get_energy_range()
         supper = s.upper(68, srange) * srange ** 2
         slower = s.lower(68, srange) * srange ** 2
         label = f"{s.journal} ({s.year})"
-        ax.fill_between(srange, slower, supper, alpha=0.3, label=label, fc="none")
+        ax.fill_between(srange, slower, supper, label=label, fc=fc[i], ec=ec[i], zorder=1, lw=2, alpha=alpha[i], ls=ls[i])
     ax.set_xscale("log")
     ax.set_yscale("log")
     ax.set_xlabel("Energy [GeV]")
-    ax.set_ylabel(r"$E^2 \frac{dN}{dE}$ [GeV cm$^{-2}$ s$^{-1}$ sr$^{-1}$]")
+    ax.set_ylabel(r"$E^2 \Phi^{\nu + \bar{\nu}}_\mathrm{per \; flavor}$ [GeV cm$^{-2}$ s$^{-1}$ sr$^{-1}$]")
     ax.legend()
     return fig
