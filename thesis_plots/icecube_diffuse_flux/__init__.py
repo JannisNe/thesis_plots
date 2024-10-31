821050b6a55a9275b726adbe6a4ed36336e7699c
JannisNe <necker@physik.hu-berlin.de>
2024-10-31 17:31:06 +0100

    add annotation

--- thesis_plots/icecube_diffuse_flux/__init__.py
+++ thesis_plots/icecube_diffuse_flux/__init__.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from thesis_plots.icecube_diffuse_flux.spectrum import Spectrum
 
 
-def load_spectrum(name):
+def load_spectrum(name: str) -> Spectrum:
     summary_file = Path(__file__).parent / "measurements.json"
     with summary_file.open("r") as f:
         data = json.load(f)
